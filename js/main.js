87dbe65e842df6c5857ba61d87944380e661f6de
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-10 04:16:40 -0600

    Add event listeners to editorial elements

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-const $skills = $('#skills > ul');
+$('.fa-bars').on('click', showMenu);
 
-const stored = [];
-
-$('#edit').on('click', editList);
+$('#new').on('click', showInput);
 $('#add').on('click', appendSkill);
+$('#check').on('click', closeInput);
+$('#delete').on('click', editList);
+$('#save').on('click', saveList);
 
-function appendSkill() {
-    let skill = $('input').val();
-    $skills.append(`<li class="item" style="font-size: 1.5rem; color: #f1f0ea"><span class="delete">x</span>${skill}</li>`);
-    stored.push(skill);
+function showMenu() {
+    
+        $('#menu').css("height", '200px').slideDown('slow');
+        $('#options').css("visibility", "visible");
+    
 }
 
-function editList(e) {
-    $('.delete').css('visibility', 'visible');
+function showInput() {
+    $('#new-skills').css("display", "inline-block");
+}
 
-    $('body').on('click', $('edit').off('click', editList));
+function appendSkill() {
+    const skill = $('input').val();
+    $('#skill-list').append(`<li><i id="trash" class="fas fa-trash-alt"></i>  ${skill}</li>`);
+}
+
+function closeInput() {
+    $('#new-skills').css("display", "none");
+}
+
+function editList() {
+    $('#trash').css("visibility", "visible");
+    $('#trash').on("click", function() {
+        $('#skill-list > li').remove();
+        $('#skill-list > .fa-trash-alt').remove();
+    })
 }
